106ee97febe886ba09b6e5be71713b0492345083
rm <jonas@jordskepp.org>
2013-04-25 12:23:51 +0200

    Revert "Kohana version control added"
    This reverts commit 28a1ae762adbf558131604603fe37f93373427c0.

--- init.php
+++ init.php
@@ -3,14 +3,6 @@
 // Set version
 define('PAJAS_XSLT_VERSION', '1.0');
 
-// Compare kohana version, must be 3.2.x to be compatible
-if (
-	version_compare(Kohana::VERSION, '3.2', '<')
-	|| version_compare(Kohana::VERSION, '3.3', '>=')
-)
-	throw new Kohana_Exception('Kohana version 3.2.x required, current version is :kohana_version',
-		array(':kohana_version', Kohana::VERSION));
-
 // Check and set up user content directory
 if ( ! is_writable(Kohana::$config->load('user_content.dir')))
 {
