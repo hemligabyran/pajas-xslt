4d0b2c4f3d848f864071fd98ebb92e9e520614bc
Lilleman <lilleman@larvit.se>
2013-04-28 12:13:18 +0200

    Removed server side browser detection... this should be done client side if needed

--- classes/pajas/xsltcontroller.php
+++ classes/pajas/xsltcontroller.php
@@ -114,7 +114,6 @@ abstract class Pajas_Xsltcontroller extends Controller
 				'url_params'  => $url_params,
 				'post_params' => $_POST,
 				'is_ajax'     => ($this->request->is_ajax()) ? 'true' : 'false',
-				'browser'     => array('name' => Request::user_agent('browser'), 'version' => Request::user_agent('version')),
 			),
 			$this->xml_meta
 		);
